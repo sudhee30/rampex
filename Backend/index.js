f16a79cce836b2fc7b0db2773a4f3f7649f582b8
Sudheeksha <sudheekshasenthilkumar@gmail.com>
2025-01-03 14:21:09 +0530

    Backend

--- Backend/index.js
+++ Backend/index.js
@@ -1,16 +1,59 @@
-//server creation code
 var express = require('express')
+var path = require('path')
+var mdb = require('mongoose')
+var User = require('./models/users')
 var app = express()
 const PORT = 3001
+app.use(express.json())
+mdb.connect("mongodb://127.0.0.1:27017/kec").then(() => {
+    console.log("MongoDB connection successful")
+}).catch(() => {
+    console.log("Check your connection string")  //connection string is the url to connect to mongodb
+})
+
 app.get('/', (req, res) => {
-    res.send("Welcome")
+    res.send("Welcome to Backend Server")
 })
-app.get('/json',(req,res)=>{
-    res.json({server:"Welcome to Backend",url:"Loclhost",port:{PORT}})
+
+app.get('/json', (req, res) => {
+    res.json("Welcome to Backend")
 })
-app.get('/static',(req,res)=>{
-    res.sendFile('C:\Users\Sudheeksha\Documents\placement\index.html')
+
+app.get('/static', (req, res) => {
+    console.log("Running")
+    res.sendFile(path.join(__dirname, 'index.html'))
 })
+
+app.post('/signup', (req,res) => {
+    console.log(req.body)
+    var {firstName, lastName, email} = req.body
+    console.log(firstName, lastName, email)
+    try{
+        var newUser = new User({
+        firstName:firstName,
+        lastName:lastName,
+        email:email
+        })
+        newUser.save()
+        console.log("User Added Successfully")
+        res.status(200).send("User Added Succussfully")
+    }
+    catch(err){
+        console.log(err)
+    }
+})
+app.get('/getsignup',async(req,res)=>{
+    try{
+        var allSignUpRecords = await User.find()
+        res.json(allSignUpRecords)
+        console.log("All data fetched")
+    }
+    catch(err){
+        console.log("Cannot able to read the records")
+        res.send(err)
+    }
+})
+
 app.listen(PORT, () => {
-    console.log(`Backend Server Started\nUrl: http://localhost:${PORT}`)
+    console.log(`Backend Server Started \n Url: http://localhost:${PORT}`) //listen should always be at the last
 })
---
+++ Backend/models/users.js
@@ -0,0 +1,8 @@
+var mdb = require('mongoose')
+var userSchema = mdb.Schema({
+    firstName:String,
+    lastName:String,
+    email:String
+})
+var user_schema=mdb.model("users",userSchema)
+module.exports=user_schema
